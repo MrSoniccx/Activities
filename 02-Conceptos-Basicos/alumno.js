5f7fc4e9ee446844b0f0487e1d42b0558c0c2990
20161012 LEONARDO YAHEL ISORDIA AVILA <20161012@TELEMATICA.UCOL.MX>
2020-02-05 18:50:06 -0600

    cuarto

--- 02-Conceptos-Basicos/alumno.js
+++ 02-Conceptos-Basicos/alumno.js
@@ -5,13 +5,37 @@ la clase empieza con mayus
 */
 class Alumno {
     /*
+    Metodo constructor
+    -sirve para incializar una clase
+    -se ejecuta de maneara automatica al crear un 
+    objeto o una instancia de la clase (new)
+*/
+    constructor(nombre,noCuenta,fecha) {
+        this.nombre = nombre
+        this.noCuenta = noCuenta
+        this.fecha = fecha
+        console.log("El objeto se ha inicializado")
+    }
+
+
+    /*
     Metodos=Asociados a clases
     -solo realizan una tarea
     -nombre del metodo es un verbo en infinitvo 
     -el nombreSeEscribeAsi
     */
     decirHola(){
-        console.log("Hola soy Yahel, Buen dia!")
+        console.log("Hola soy "+this.nombre+", Buen dia!")
+        console.log(`Hola soy ${this.nombre}, Buen dia!`)
+    }
+    decirAdios(){
+        console.log(`Adios! ${this.nombre} se a retirado`)
+    }
+
+    mostrarPerfil(){
+        console.log(`   ~ Nombre = ${this.nombre}`)
+        console.log(`   ~ No. de cuenta = ${this.noCuenta}`)
+        console.log(`   ~ Fecha de nacimiento = ${this.fecha}`)
     }
 }
 /*
@@ -20,8 +44,13 @@ class Alumno {
 -El operador new se utiliza para crear un nuevo
 objeto o una nueva instancia de una clase
 */
-let alumno1 = new Alumno();
-let alumno2 = new Alumno();
+let alumno1 = new Alumno("pepe","20161012",new Date (2002, 3, 17))
+let alumno2 = new Alumno("juan","20145754",new Date (2000, 1, 1))
 
-alumno1.decirHola();
-alumno2.decirHola();
+alumno1.decirHola()
+alumno1.mostrarPerfil()
+alumno1.decirAdios()
+
+alumno2.decirHola()
+alumno2.mostrarPerfil()
+alumno2.decirAdios()
