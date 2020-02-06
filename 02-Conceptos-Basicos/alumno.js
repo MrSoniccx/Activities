ef7a691fa836b90f53799dcf18f0dfd56de1ffe0
20161012 LEONARDO YAHEL ISORDIA AVILA <20161012@TELEMATICA.UCOL.MX>
2020-02-05 18:08:40 -0600

    tercer

--- 02-Conceptos-Basicos/alumno.js
+++ 02-Conceptos-Basicos/alumno.js
@@ -1,5 +1,27 @@
+//clases
 /*Nombre de la clase y el archivo son los mismos
 El nombre de clase es en singular
 la clase empieza con mayus
 */
-class Alumno {}
+class Alumno {
+    /*
+    Metodos=Asociados a clases
+    -solo realizan una tarea
+    -nombre del metodo es un verbo en infinitvo 
+    -el nombreSeEscribeAsi
+    */
+    decirHola(){
+        console.log("Hola soy Yahel, Buen dia!")
+    }
+}
+/*
+-alumno1 es un objeto de tipo Alumno
+-alumno1 es una instancia de la clase Alumno 
+-El operador new se utiliza para crear un nuevo
+objeto o una nueva instancia de una clase
+*/
+let alumno1 = new Alumno();
+let alumno2 = new Alumno();
+
+alumno1.decirHola();
+alumno2.decirHola();
